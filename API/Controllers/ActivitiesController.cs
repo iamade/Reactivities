22a58765548d6b3cb685c4bba2b332758745eab5
Adesegun Koiki <kamar3deen@gmail.com>
2024-02-27 16:51:36 +0100

    end of Section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,32 +1,45 @@
-using Persistence;
+
 using Microsoft.AspNetCore.Mvc;
 using Domain;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
+using Application.Activities;
 
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-       
-        public ActivitiesController(DataContext context) 
-        {
-            _context = context;
-
-        }
+      
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>CreateActivity(Activity activity) {
+            await Mediator.Send(new Create.Command{ Activity = activity});
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(Guid id, Activity activity) {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command {Activity = activity});
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id) {
+            await Mediator.Send(new Delete.Command{Id = id});
+            return Ok();
         }
     }
 }
---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,16 @@
+
+
+using System.Diagnostics;
+using AutoMapper;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles(){
+
+        CreateMap<Activity, Activity>();
+        }
+
+    }
+}
