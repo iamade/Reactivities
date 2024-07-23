c113b8eec838fde8c5fb74b23e4c22921ac661bd
Adesegun Koiki <kamar3deen@gmail.com>
2024-07-23 16:40:17 -0600

    stooped at 49 creatina an activity

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid, GridColumn, List } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activities: Activity[];
+}
+export default function ActivityDashboard({activities}: Props) {
+  return (
+    <Grid>
+      <GridColumn width="10">
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </GridColumn>
+    </Grid>
+  );
+}
